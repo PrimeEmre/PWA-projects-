569905ed0aebb2f7ca108c4f1fed02ca790d89d5
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2025-11-29 21:17:10 -0500

    instaled the npm

--- public/JS/serviceWorker.js
+++ public/JS/serviceWorker.js
@@ -1,22 +1,26 @@
-let assets = [
-    '/',
-    'index.html',
-    'script.js',
-]
+const assets = [
+    './',
+    './index.html',
+    './CSS/style.css',   
+    './js/script.js',    // Make sure this matches your folder name (JS vs js)
+    // './images/AI-genrate.jpg'
+];
 
-self.addEventListener("install") , function(installEvent){
+// FIXED: Removed the extra comma outside the function
+self.addEventListener("install", function(installEvent) {
     installEvent.waitUntil(
-        caches.open("ai-generator-pwa").then(function(caches){
-            caches.addAll(assets)
+        caches.open("ai-generator-pwa").then(function(cache) {
+            return cache.addAll(assets);
         })
-    )
-}
+    );
+});
 
-self.addEventListener("fetch",function(fetchEvent){
-
-    fetchEvent.reponWith(
-        caches.match(fetchEvent.request).then(function (res){
-            return res || fetch(fetchEvent).request
+self.addEventListener("fetch", function(fetchEvent) {
+    // FIXED: Spelling 'respondWith'
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(function(res) {
+            // FIXED: fetch(fetchEvent.request)
+            return res || fetch(fetchEvent.request);
         })
-    )
-})
+    );
+});
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-let express = require("express")
-let path = require ("path")
+let express = require("express");
+let path = require("path");
 
-let app =express()
-let port = 8000
-app.use(express.static(path.join(__dirname,"public")))
+let app = express();
+let port = 8000;
 
-app.get
+// Serve static files (css, js, images) from the public folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Fix: Changed "x" to "/" so it loads on the homepage
+app.get("/", function (req, res) {
+    // Fix: Removed the comma inside the string
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(port, function () {
+    console.log("Server on port " + port);
+});
